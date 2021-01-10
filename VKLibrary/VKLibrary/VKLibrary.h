1d38b86c5956e514b01fcc15217475c61edf99e7
VK <Vaisakh Krishnan>
2021-01-10 13:51:52 +0800

    Initial commit

---
+++ VKLibrary/VKLibrary/VKLibrary.h
@@ -0,0 +1,18 @@
+//
+//  VKLibrary.h
+//  VKLibrary
+//
+//  Created by User on 1/10/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for VKLibrary.
+FOUNDATION_EXPORT double VKLibraryVersionNumber;
+
+//! Project version string for VKLibrary.
+FOUNDATION_EXPORT const unsigned char VKLibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VKLibrary/PublicHeader.h>
+
+
---
+++ VKLibrary/VKLibraryExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VKLibraryExample
+//
+//  Created by User on 1/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
